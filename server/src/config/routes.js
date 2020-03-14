f1917e9f1b7bb3351ada5af9dc81adc7a38c4cba
gabrielurena <gabriel.uh@hotmail.com>
2020-03-14 15:50:58 -0400

    adds teams functionality

--- server/src/config/routes.js
+++ server/src/config/routes.js
@@ -1,5 +1,6 @@
 import questions from '../rest/components/question/routes';
 import categories from '../rest/components/category/routes';
 import competitors from '../rest/components/competitor/routes';
+import teams from '../rest/components/team/routes';
 
-export { questions, categories, competitors };
+export { questions, categories, competitors, teams };
---
+++ server/src/rest/components/team/model.js
@@ -0,0 +1,49 @@
+import moongose from 'mongoose';
+import Joi from '@hapi/joi';
+
+const Schema = moongose.Schema;
+
+const Team = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 255,
+  },
+  competitors: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Competitor',
+    },
+  ],
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+/**
+ * Validate the data sent to create a team.
+ * @param {Object} team
+ * @returns {Promise} Promise-like object that
+ * can be used as a promise, or as a simple object.
+ *
+ * To learn more you can head to:
+ * https://github.com/hapijs/joi/blob/v13.1.2/API.md#validatevalue-schema-options-callback
+ */
+export function validateTeam(team) {
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(4)
+      .max(255)
+      .required(),
+    competitors: Joi.array()
+      .items(Joi.objectId())
+      .required(),
+  }).options({ stripUnknown: true });
+
+  return schema.validate(team);
+}
+
+export default moongose.model('Team', Team);
