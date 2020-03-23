2554ebb0c218c950d40e1de8e3785232c92b5918
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-03-23 19:06:04 -0400

    adds countdown timer

--- app/src/containers/Game/index.jsx
+++ app/src/containers/Game/index.jsx
@@ -1,9 +1,110 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import axios from "axios";
+import { Card, Modal, Button } from "antd";
+import Countdown from "react-countdown-now";
+import { URL_ROUNDS, URL_QUESTIONS } from "../../config/urls";
 
 class Game extends Component {
+  state = {
+    questions: [],
+    question: 0,
+    loading: true,
+    visible: false,
+    published: false
+  };
+
+  componentDidMount() {
+    axios
+      .get(`${URL_ROUNDS}/${this.props.match.params.idOfRound}`)
+      .then(({ data }) =>
+        this.setState({ questions: data.round.questions, loading: false })
+      )
+      .catch(({ response }) => console.log(response));
+  }
+
+  showModal = selectedQuestion => {
+    this.setState({ visible: true, question: selectedQuestion });
+  };
+
+  handleOk = () => {
+    this.setState({
+      visible: false
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  publishQuestion = event => {
+    axios
+      .post(`${URL_QUESTIONS}/publish/`, { question: event.target.value })
+      .then(res => {
+        console.log(res);
+
+        if (res.status === 200) {
+          this.setState({ published: true });
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
+  renderer = ({ seconds }) => <span>{seconds}</span>;
+
   render() {
-    console.log(this.props.match.params.idOfRound);
-    return <h1>Let the games begin!</h1>;
+    const gridStyle = {
+      width: "25%",
+      textAlign: "center"
+    };
+
+    const { loading, questions, visible, question, published } = this.state;
+
+    return (
+      <div>
+        {loading ? (
+          <Card loading={true}></Card>
+        ) : (
+          <Fragment>
+            <Card loading={loading}>
+              {questions.map((question, index) => {
+                return (
+                  <Card.Grid
+                    style={gridStyle}
+                    key={question._id}
+                    onClick={() => this.showModal(index)}
+                  >
+                    {question.points}
+                  </Card.Grid>
+                );
+              })}
+            </Card>
+            <Modal
+              visible={visible}
+              onOk={this.handleOk}
+              onCancel={this.handleCancel}
+              centered
+              footer={[
+                <Button
+                  key="submit"
+                  value={questions[question].name}
+                  onClick={this.publishQuestion}
+                >
+                  Abrir Pregunta
+                </Button>
+              ]}
+            >
+              {questions[question].name}
+              <br />
+              {published ? (
+                <Countdown date={Date.now() + 15000} renderer={this.renderer} />
+              ) : (
+                <Fragment />
+              )}
+            </Modal>
+          </Fragment>
+        )}
+      </div>
+    );
   }
 }
 
