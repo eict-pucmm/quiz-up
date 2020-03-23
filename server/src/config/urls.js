6c2c92cee2a2120b7382898bc2b43544e4d36480
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-03-23 00:37:57 -0400

    adds config folder to client side

--- server/src/config/urls.js
+++ server/src/config/urls.js
@@ -1,9 +1,9 @@
-const URL_QUESTIONS = '/api/questions/';
-const URL_CATEGORIES = '/api/categories/';
-const URL_COMPETITORS = '/api/competitors/';
-const URL_TEAMS = '/api/teams/';
-const URL_EVENTS = '/api/events/';
-const URL_ROUNDS = '/api/rounds/';
+const URL_QUESTIONS = '/api/questions';
+const URL_CATEGORIES = '/api/categories';
+const URL_COMPETITORS = '/api/competitors';
+const URL_TEAMS = '/api/teams';
+const URL_EVENTS = '/api/events';
+const URL_ROUNDS = '/api/rounds';
 const URL_RESULTS = '/api/results';
 
 export {
