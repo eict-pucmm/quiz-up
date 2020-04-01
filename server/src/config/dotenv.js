868273c31806c8d9dfe0051c601f9361fa02997c
Gabriel Arturo Urena <gurena@intellisys.com.do>
2020-04-01 00:29:15 -0400

    adds client in container for development

--- server/src/config/dotenv.js
+++ server/src/config/dotenv.js
@@ -2,8 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const AMQP_CONNECTION_URL =
-  process.env.AMQP_CONNECTION_URL || 'http://localhost:5672';
+const AMQP_CONNECTION_URL = process.env.AMQP_CONNECTION_URL;
 const MONGO = process.env.MONGO_URI || process.env.MONGO_DOCKER_URI;
 const PORT = process.env.PORT || 3000;
 
