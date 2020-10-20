7735fbf881b9699eee50f5cabb8b44783bc0c29f
Javier Rojas <jrojas03.08.98@gmail.com>
2020-10-20 19:13:25 -0400

    some fixes on question bank

--- server/src/rest/components/questionsBank/controller.js
+++ server/src/rest/components/questionsBank/controller.js
@@ -17,8 +17,14 @@ const create = async (req, res) => {
       categories: { $regex: regex },
     })
   );
+  const orderedByCategories = {
+    [categories[0]]: [],
+    [categories[1]]: [],
+    [categories[2]]: [],
+    [categories[3]]: [],
+  };
 
-  const qbank = createQuestionBank(questions);
+  const qbank = createQuestionBank(questions, orderedByCategories);
 
   return error
     ? res
@@ -27,37 +33,100 @@ const create = async (req, res) => {
     : res.status(CREATED).json({ qbank });
 };
 
-const createQuestionBank = questions => {
+const createQuestionBank = (questions, categories) => {
   let orderedPoints = { oneH: [], twoH: [], threeH: [], fourH: [], fiveH: [] };
   questions.forEach((question, index) => {
-    let arr = [];
-    console.log('QUESTIONS !!!!', question);
-    if (question.points == 100) {
-      orderedPoints.oneH.push(question);
-    } else if (question.points == 200) {
-      orderedPoints.twoH.push(question);
-    } else if (question.points == 300) {
-      orderedPoints.threeH.push(question);
-    } else if (question.points == 400) {
-      orderedPoints.fourH.push(question);
-    } else if (question.points == 500) {
-      orderedPoints.fiveH.push(question);
-    }
-
-    console.log('>>>>>>>>>>>>>>>>>\n', orderedPoints.oneH);
+    Object.keys(categories).forEach(key => {
+      if (question.categories.includes(key)) {
+        let objFormat = {
+          name: question.name,
+          points: question.points,
+          category: question.categories,
+          _id: question._id,
+        };
+        categories[key].push(objFormat);
+      }
+    });
   });
-  console.table(orderedPoints);
-  return orderedPoints;
+
+  return pickRandomQuestion(categories);
 };
 
 /* THIS FUNCTION WILL HELP GET ONE RANDOM QUESTION FROM THE ORDERED BY POINTS QUESTIONS*/
 
-// function pickRandomQuestion() {
-//     var obj_keys = Object.keys(window.questionnaire);
-//     var ran_key = obj_keys[Math.floor(Math.random() * obj_keys.length)];
-//     window.selectedquestion = window.questionnaire[ran_key];
-//     console.log(window.selectedquestion);
-//     console.log(window.questionnaire);
-// }
+function pickRandomQuestion(categories) {
+  console.log(categories);
 
+  let questionBank = {};
+  let actualCatQuestions = {
+    oneH: [],
+    twoH: [],
+    threeH: [],
+    fourH: [],
+    fiveH: [],
+  };
+
+  Object.keys(categories).forEach(key => {
+    questionBank[key] = {
+      oneH: [],
+      twoH: [],
+      threeH: [],
+      fourH: [],
+      fiveH: [],
+    };
+  });
+
+  for (let [key, val] of Object.entries(categories)) {
+    val.forEach(question => {
+      if (question.points == 100) {
+        actualCatQuestions.oneH.push(question);
+      } else if (question.points == 200) {
+        actualCatQuestions.twoH.push(question);
+      } else if (question.points == 300) {
+        actualCatQuestions.threeH.push(question);
+      } else if (question.points == 400) {
+        actualCatQuestions.fourH.push(question);
+      } else if (question.points == 500) {
+        actualCatQuestions.fiveH.push(question);
+      }
+    });
+
+    questionBank[key].oneH =
+      actualCatQuestions.oneH[
+        Math.floor(Math.random() * actualCatQuestions.oneH.length)
+      ];
+    questionBank[key].twoH =
+      actualCatQuestions.twoH[
+        Math.floor(Math.random() * actualCatQuestions.twoH.length)
+      ];
+    questionBank[key].threeH =
+      actualCatQuestions.threeH[
+        Math.floor(Math.random() * actualCatQuestions.threeH.length)
+      ];
+    questionBank[key].fourH =
+      actualCatQuestions.fourH[
+        Math.floor(Math.random() * actualCatQuestions.fourH.length)
+      ];
+    questionBank[key].fiveH =
+      actualCatQuestions.fiveH[
+        Math.floor(Math.random() * actualCatQuestions.fiveH.length)
+      ];
+    console.log('SUPPOSED QUESTION SELECTED >>>>\n\n', questionBank);
+
+    actualCatQuestions = {
+      oneH: [],
+      twoH: [],
+      threeH: [],
+      fourH: [],
+      fiveH: [],
+    };
+  }
+
+  console.log(
+    '||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n'
+  );
+  console.log(questionBank);
+
+  return questionBank;
+}
 export { create };
