4181d889ee8e59f04635872e9736fb3955f3d7b8
gabriel urena <gabriel.uh@hotmail.com>
2020-10-27 20:03:35 -0400

    changes styles on login component

--- app/src/containers/Login/index.jsx
+++ app/src/containers/Login/index.jsx
@@ -1,53 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Breadcrumb,
-  Button,
-  Input,
-  Form,
-} from 'antd';
+import React, { useState } from 'react';
+import { Modal, Button, Input, Form } from 'antd';
 
-
-import { auth , provider } from '../../constants/firebase'
+import { auth } from '../../constants/firebase';
 
 const Login = () => {
-    const [form] = Form.useForm();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(false);
+  const [form] = Form.useForm();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
 
-     const signInWithEmailAndPasswordHandler = (event, email, password) => {
-        event.preventDefault();
-        auth.signInWithEmailAndPassword(email, password)
-        .then( async res => {              
-            localStorage.setItem('USER', res.user)
-            window.location.replace("/")
-            
-        })
-        .catch(error => {
-            setError("Error signing in with password and email!");
-            console.error("Error signing in with password and email", error);
-        });
-     }
+  const signInWithEmailAndPasswordHandler = (event, email, password) => {
+    event.preventDefault();
 
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(async res => {
+        localStorage.setItem('USER', res.user.getIdToken(true));
+        window.location.replace('/');
+      })
+      .catch(error => {
+        setError(true);
+      });
+  };
 
   return (
-    <>
-      <Breadcrumb className="breadcrumb-title">
-        <Breadcrumb.Item>Iniciar Sesión</Breadcrumb.Item>
-      </Breadcrumb>
-      {error && 
-            <p style={{color:"red"}}>{error}</p>
-        }
+    <Modal
+      visible
+      footer={null}
+      title="Iniciar Sesion - Quiz Up"
+      maskStyle={{
+        background: 'linear-gradient(to bottom, #2980b9, #6dd5fa, #ffffff)',
+      }}>
+      {error && (
+        <p style={{ color: 'red' }}>¡Clave o nombre de usuario incorrecto!</p>
+      )}
       <Form
         form={form}
-        layout="horizontal"
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 8 }}>
-        
+        layout="vertical"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 0 }}>
         <Form.Item label="Correo: ">
           <Input
             value={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={e => setEmail(e.target.value.trim())}
             name="email"
           />
         </Form.Item>
@@ -59,16 +54,19 @@ const Login = () => {
             onChange={e => setPassword(e.target.value)}
           />
         </Form.Item>
-
-        <Form.Item wrapperCol={{ span: 14, offset: 4 }}>
-          <Button key="submit" type="primary" onClick={(event) => {signInWithEmailAndPasswordHandler(event, email, password)}}>
+        <Form.Item wrapperCol={{ span: 14 }}>
+          <Button
+            key="submit"
+            type="primary"
+            onClick={event => {
+              signInWithEmailAndPasswordHandler(event, email, password);
+            }}>
             Iniciar Sesión
           </Button>
         </Form.Item>
       </Form>
-
-    </>
+    </Modal>
   );
 };
 
-export default Login
+export default Login;
