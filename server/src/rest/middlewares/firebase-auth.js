5624dd3dc78800294dfb48963f748119d3c34a6c
gabriel urena <gabriel.uh@hotmail.com>
2020-10-28 02:49:14 -0400

    add firebase-auth middleware

---
+++ server/src/rest/middlewares/firebase-auth.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase-admin';
+import { FORBIDDEN, UNAUTHORIZED } from '../../config/statusCodes';
+
+firebase.initializeApp({
+  //credentials: GET_CREDENTIALS_FROM_FIREBASE
+  databaseURL: 'https://quizup-c36c7.firebaseio.com',
+});
+
+const authMiddleware = (request, response, next) => {
+  const headerToken = request.headers.authorization;
+  if (!headerToken) {
+    return response.send({ message: 'No token provided' }).status(UNAUTHORIZED);
+  }
+
+  if (headerToken && headerToken.split(' ')[0] !== 'Bearer') {
+    response.send({ message: 'Invalid token' }).status(UNAUTHORIZED);
+  }
+
+  const token = headerToken.split(' ')[1];
+  firebase
+    .auth()
+    .verifyIdToken(token)
+    .then(() => next())
+    .catch(() =>
+      response.send({ message: 'Could not authorize' }).status(FORBIDDEN)
+    );
+};
+
+export default authMiddleware;
