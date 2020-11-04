e8b58556003a1d23779766753882571962e3b00c
gabriel urena <gabriel.uh@hotmail.com>
2020-11-04 18:28:13 -0400

    changes baseUrl on axios client

--- pwa/src/api/axios.js
+++ pwa/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getUser } from './user';
 
 export const apiClient = axios.create({
-  baseURL: '/',
+  baseURL: 'http://3.20.168.127:8080/',
   headers: {
     'Content-Type': 'application/json',
     Authorization: 'Bearer ' + getUser(),
