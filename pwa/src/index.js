86ceb39dd136c371136f6ee31f0c759a15d0fe3b
gabriel urena <gabriel.uh@hotmail.com>
2020-11-27 10:23:12 -0400

    removes serviceWorker

--- pwa/src/index.js
+++ pwa/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { StateProvider } from './state';
 import { initialState } from './state/initialState';
 import { reducer } from './state/reducer';
@@ -15,10 +14,3 @@ ReactDOM.render(
   </StateProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-process.env.NODE_ENV === 'dev'
-  ? serviceWorker.unregister()
-  : serviceWorker.register();
